79108d478e1598ed3ce9929c9b19314528be5d74
James Uanhoro <stonegold546@gmail.com>
2015-11-04 07:07:07 +0800

    Async implementation works!

--- future.rb
+++ future.rb
@@ -4,25 +4,27 @@ require 'benchmark'
 
 sites = %w(cnn facebook google microsoft github)
 
-def retrieve_all(sites)
-  sites.map { |site| [site, HTTParty.get("http://#{site}.com")] }.to_h
+def retrieve_all(sites, n)
+  (sites * n).map do |site|
+    [site, HTTParty.get("http://#{site}.com")]
+  end.to_h
 end
 
-def retrieve_all_future(sites)
-  sites.map do |site|
+def retrieve_all_future(sites, n)
+  (sites * n).map do |site|
     Concurrent::Future.new { [site, HTTParty.get("http://#{site}.com")] }
   end.map(&:execute).map(&:value).to_h
 end
 
-def retrieve_all_promise(sites)
-  sites.map do |site|
+def retrieve_all_promise(sites, n)
+  (sites * n).map do |site|
     Concurrent::Promise.new { [site, HTTParty.get("http://#{site}.com")] }
   end.map(&:execute).map(&:value).to_h
 end
 
 n = ARGV[0].nil? ? 1 : ARGV[0].to_i
 Benchmark.bmbm(7) do |bench|
-  bench.report('sync:') { n.times { retrieve_all(sites) } }
-  bench.report('future:') { n.times { retrieve_all_future(sites) } }
-  bench.report('promise:') { n.times { retrieve_all_promise(sites) } }
+  bench.report('sync:') { retrieve_all(sites, n) }
+  bench.report('future:') { retrieve_all_future(sites, n) }
+  bench.report('promise:') { retrieve_all_promise(sites, n) }
 end
