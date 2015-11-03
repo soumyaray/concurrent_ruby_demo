c96141c58624440ed9a06189ec780d60640144f8
James Uanhoro <stonegold546@gmail.com>
2015-11-03 21:53:53 +0800

    Added additional asynchronous methods
    - Found no difference between Future & Promise
    - Sometimes, the asynchronous mechanisms break down. Often enough to appear in the 10-15 tests I ran.

--- future.rb
+++ future.rb
@@ -8,13 +8,21 @@ def retrieve_all(sites)
   sites.map { |site| [site, HTTParty.get("http://#{site}.com")] }.to_h
 end
 
-def retrieve_all_async(sites)
+def retrieve_all_future(sites)
   sites.map do |site|
     Concurrent::Future.new { [site, HTTParty.get("http://#{site}.com")] }
   end.map(&:execute).map(&:value).to_h
 end
 
+def retrieve_all_promise(sites)
+  sites.map do |site|
+    Concurrent::Promise.new { [site, HTTParty.get("http://#{site}.com")] }
+  end.map(&:execute).map(&:value).to_h
+end
+
+n = ARGV[0].nil? ? 1 : ARGV[0].to_i
 Benchmark.bmbm(7) do |bench|
-  bench.report('sync:') { retrieve_all(sites) }
-  bench.report('async:') { retrieve_all_async(sites) }
+  bench.report('sync:') { n.times { retrieve_all(sites) } }
+  bench.report('future:') { n.times { retrieve_all_future(sites) } }
+  bench.report('promise:') { n.times { retrieve_all_promise(sites) } }
 end
