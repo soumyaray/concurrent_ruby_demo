7cefca859c355badd93b2f994973ee88e4b021cc
Soumya Ray <soumya.ray@gmail.com>
2015-11-04 13:32:12 +0800

    future.rb: move args check to top, renamed parameter

--- future.rb
+++ future.rb
@@ -3,28 +3,28 @@ require 'httparty'
 require 'benchmark'
 
 sites = %w(cnn facebook google microsoft github)
+repeat = ARGV[0].nil? ? 1 : ARGV[0].to_i
 
-def retrieve_all(sites, n)
-  (sites * n).map do |site|
+def retrieve_all(sites, repeat)
+  (sites * repeat).map do |site|
     [site, HTTParty.get("http://#{site}.com")]
   end.to_h
 end
 
-def retrieve_all_future(sites, n)
-  (sites * n).map do |site|
+def retrieve_all_future(sites, repeat)
+  (sites * repeat).map do |site|
     Concurrent::Future.new { [site, HTTParty.get("http://#{site}.com")] }
   end.map(&:execute).map(&:value).to_h
 end
 
-def retrieve_all_promise(sites, n)
-  (sites * n).map do |site|
+def retrieve_all_promise(sites, repeat)
+  (sites * repeat).map do |site|
     Concurrent::Promise.new { [site, HTTParty.get("http://#{site}.com")] }
   end.map(&:execute).map(&:value).to_h
 end
 
-n = ARGV[0].nil? ? 1 : ARGV[0].to_i
 Benchmark.bmbm(7) do |bench|
-  bench.report('sync:') { retrieve_all(sites, n) }
-  bench.report('future:') { retrieve_all_future(sites, n) }
-  bench.report('promise:') { retrieve_all_promise(sites, n) }
+  bench.report('sync:') { retrieve_all(sites, repeat) }
+  bench.report('future:') { retrieve_all_future(sites, repeat) }
+  bench.report('promise:') { retrieve_all_promise(sites, repeat) }
 end
