12ec7307b59be0c6a217aad7627bbaaed631bd05
Soumya Ray <soumya.ray@gmail.com>
2024-12-01 19:36:47 +0800

    fix: add delay to race condition demo

--- threads.rb
+++ threads.rb
@@ -7,41 +7,58 @@ class BankAccount
   end
 
   def debit(amount)
-    @balance -= amount
+    new_amount = @balance - amount
+    update_server(new_amount)
+    @balance = new_amount
   end
 
   def credit(amount)
-    @balance += amount
+    new_amount = @balance + amount
+    update_server(new_amount)
+    @balance = new_amount
+  end
+
+  def update_server(new_amount)
+    sleep 0.000001 # simulate a small networking delay
   end
 end
 
 thread_count = 100
-iterate = 1_000_000
+iterate = 200
 total = thread_count * iterate
 
+## Synchronous crediting
 account = BankAccount.new(0)
-
 puts "Starting balance is #{account.balance}."
 
-## Synchronous crediting
-# puts "Crediting account using a single thread, #{total} times."
-# total.times do
-#   account.half += 1 if account.balance.even?
-#   account.credit(1)
-# end
+puts "Crediting account using a single thread, #{total} times."
+total.times do
+  account.half += 1 if account.balance.even?
+  account.credit(1)
+  print '.' if account.balance % 100 == 0
+end
+
+puts
+puts "Account balance is now #{account.balance}."
+puts "Half balance is now #{account.half}."
 
 ## Multithreaded crediting
+account = BankAccount.new(0)
+puts "Starting balance is #{account.balance}."
+
 puts "Crediting account using #{thread_count} threads, #{iterate} times each."
 require 'thread'
-threads = (0...thread_count).map do
+threads = thread_count.times.map do
   Thread.new do
     iterate.times do
       account.half += 1 if account.balance.even?
       account.credit(1)
+      print '.' if account.balance % 100 == 0
     end
   end
 end
 threads.each(&:join)
 
+puts
 puts "Account balance is now #{account.balance}."
 puts "Half balance is now #{account.half}."
